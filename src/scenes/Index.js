d01650e683f25ad7d26e2275bc566d5ed7d8bcf4
Artur <artur.h.0427@gmail.com>
2022-11-12 16:50:57 +0100

    Restructure public folder & add profile icons

--- src/scenes/Index.js
+++ src/scenes/Index.js
@@ -115,16 +115,16 @@ export default class Index extends React.PureComponent {
                             <div className="stripe" />
                             <div className="stripe" />
                         </div>
-                        <img className="logo" src="./Untitled.svg"/>
+                        <img className="logo" src="./logo/untitled.svg"/>
                     </div>
 
                     <div className="container">
                         <div className="content">
                             <div className="buttons">
-                                <OutlineButton iconSrc="./enter.svg" buttonText="Sign up" additionalClass="one" href="join" />
-                                <OutlineButton iconSrc="./create.svg" buttonText="Create" additionalClass="two" href="create" />
-                                <OutlineButton iconSrc="./browse.svg" className="large" buttonText="Browse" additionalClass="three" href="browse" />
-                                <OutlineButton iconSrc="./user.svg" buttonText="Sign up" additionalClass="four" href="sign-up" />
+                                <OutlineButton iconSrc="./icons/enter.svg" buttonText="Sign up" additionalClass="one" href="join" />
+                                <OutlineButton iconSrc="./icons/create.svg" buttonText="Create" additionalClass="two" href="create" />
+                                <OutlineButton iconSrc="./icons/browse.svg" className="large" buttonText="Browse" additionalClass="three" href="browse" />
+                                <OutlineButton iconSrc="./icons/user.svg" buttonText="Sign up" additionalClass="four" href="sign-up" />
                             </div>
                         </div>
 
@@ -135,7 +135,7 @@ export default class Index extends React.PureComponent {
                         </div> */}
                     </div>
                     
-                    <img className="chevron" src="./down.svg" onClick={this.scroll}/>
+                    <img className="chevron" src="./icons/down.svg" onClick={this.scroll}/>
                 </section>
                 <section className="leaderboards" ref={this.scrollIntoViewElement}>
                     <fieldset className="container">
