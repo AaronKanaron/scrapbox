4c28e632cde177d28049e39b00455aa88db7d8b4
Aaron <aaronkanaron@gmail.com>
2022-11-12 14:23:01 +0100

    SCSS and Leaderboard.jsx changes with leaderboard

--- src/components/molecules/Leaderboard.jsx
+++ src/components/molecules/Leaderboard.jsx
@@ -3,86 +3,101 @@ import React from 'react'
 import LeaderboardItem from '../atoms/LeaderboardItem'
 
 const player = [
-    {
-        rank: 1,
-        username: 'UraniumConsumer',
-        points: 100
-    },
-    {
-        rank: 2,
-        username: 'ChildCombustor',
-        points: 50
-    },
-    {
-        rank: 3,
-        username: 'GloblingBost',
-        points: 20
-    },
-    {
-        rank: 4,
-        username: 'PotatoBuster',
-        points: 10
-    },
-    {
-        rank: 5,
-        username: 'NaseemHamed',
-        points: 10
-    }
-    ,{
-        rank: 6,
-        username: 'PotatoBuster',
-        points: 10
-    },
-    {
-        rank: 7,
-        username: 'NaseemHamed',
-        points: 10
-    }
-    ,{
-        rank: 8,
-        username: 'PotatoBuster',
-        points: 10
-    },
-    {
-        rank: 9,
-        username: 'NaseemHamed',
-        points: 10
-    }
-    ,{
-        rank: 10,
-        username: 'PotatoBuster',
-        points: 10
-    },
-    {
-        rank: 11,
-        username: 'NaseemHamed',
-        points: 10
-    }
-    ,{
-        rank: 12,
-        username: 'PotatoBuster',
-        points: 10
-    },
-    {
-        rank: 13,
-        username: 'NaseemHamed',
-        points: 10
-    }
+    // {
+    //     rank: 1,
+    //     username: 'UraniumConsumer',
+    //     points: 100
+    // },
+    // {
+    //     rank: 2,
+    //     username: 'ChildCombustor',
+    //     points: 50
+    // },
+    // {
+    //     rank: 3,
+    //     username: 'GloblingBost',
+    //     points: 20
+    // },
+    // {
+    //     rank: 4,
+    //     username: 'PotatoBuster',
+    //     points: 10
+    // },
+    // {
+    //     rank: 5,
+    //     username: 'NaseemHamed',
+    //     points: 10
+    // }
+    // ,{
+    //     rank: 6,
+    //     username: 'PotatoBuster',
+    //     points: 10
+    // },
+    // {
+    //     rank: 7,
+    //     username: 'NaseemHamed',
+    //     points: 10
+    // }
+    // ,{
+    //     rank: 8,
+    //     username: 'PotatoBuster',
+    //     points: 10
+    // },
+    // {
+    //     rank: 9,
+    //     username: 'NaseemHamed',
+    //     points: 10
+    // }
+    // ,{
+    //     rank: 10,
+    //     username: 'PotatoBuster',
+    //     points: 10
+    // },
+    // {
+    //     rank: 11,
+    //     username: 'NaseemHamed',
+    //     points: 10
+    // }
+    // ,{
+    //     rank: 12,
+    //     username: 'PotatoBuster',
+    //     points: 10
+    // },
+    // {
+    //     rank: 13,
+    //     username: 'NaseemHamed',
+    //     points: 10
+    // }
 ]
 
 
 export default class Leaderboard extends React.PureComponent {
+
+    renderLeaderboard() {
+        if (player.length > 0) {
+            return player.map((player, index) => (
+                <LeaderboardItem
+                    key={index}
+                    rank={player.rank}
+                    username={player.username}
+                    points={player.points}
+                />
+            ))
+        } else {
+            return (
+                <li className="leaderboard-item">
+                    <p>You have no friends</p>
+                </li>
+            )
+        }
+    }
+
     render() {
         return (
             <ul className="leaderboard">
-                {player.map((player, index) => (
-                    <LeaderboardItem
-                        key={index}
-                        rank={player.rank}
-                        username={player.username}
-                        points={player.points}
-                    />
-                ))}
+                {
+                    this.renderLeaderboard()
+                }
             </ul>           
         )
     }
